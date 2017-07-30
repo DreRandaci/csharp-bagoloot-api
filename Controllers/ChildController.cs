1a8b122ce6d1da5dd188cbc1dd6c28674308a712
Steve Brownlee <chortlehoort@gmail.com>
2017-07-30 17:53:06 -0500

    Injected logging service to child controller

--- Controllers/ChildController.cs
+++ Controllers/ChildController.cs
@@ -7,53 +7,93 @@ using BagAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
+// In an ASP.NET application, the namespace will often, but does not need to,
+// match the name of the directory in which the class is contained.
 namespace BagAPI.Controllers
 {
     [Route("api/[controller]")]
     public class ChildController : Controller
     {
         private BagAPIContext _context;
-        public ChildController(BagAPIContext ctx)
+        private ILogger _log;
+
+        /*
+            Dependency injection works in ASP.NET exactly like it did with Angular.
+
+            In this controller, I'm injecting...
+                1. Entity Framework database context 
+                2. The logging service, so I can write log messages without 
+                   resorting to Console.WriteLine.
+         */
+        public ChildController(BagAPIContext ctx, ILogger<ChildController> logger)
         {
             _context = ctx;
+            _log = logger;
         }
 
-        // GET api/values
+        /*
+            GET api/child
+
+            This method handles GET requests for the child resource. There
+            are two URL parameters that can be used to filter the list of 
+            children returned.
+
+                /api/child?delivered=0
+                /api/child?name=Svetlana
+                /api/child?delivered=0&name=Nigel
+         */
         [HttpGet]
-        public async Task<IActionResult> Get(string delivered, string name)
+        public async Task<IActionResult> Get(int? delivered, string name)
         {
             IEnumerable<Child> children;
 
-            (string Name, string Delivered) filter = (name, delivered);
+            // Store URL parameters in a tuple, just because I can
+            (string Name, int? Delivered) filter = (name, delivered);
 
-            if (filter.Delivered == "1")
+            // If delivered was not a parameter at all
+            if (filter.Delivered == null)
             {
-                children = await _context.Child.Include("Toys").Where(c => c.Delivered == 1).ToListAsync();
-            } else if (filter.Delivered == "0") {
-                children = await _context.Child.Include("Toys").Where(c => c.Delivered == 0).ToListAsync();
-            } else {
                 children = await _context.Child.Include("Toys").ToListAsync();
+
+            // Delivered was a parameter. Use it to filter query.
+            } else {
+                children = await _context.Child
+                        .Include("Toys")
+                        .Where(c => c.Delivered == filter.Delivered)
+                        .ToListAsync();
             }
 
+            // If name was specified, refine query further
             if (filter.Name != null)
             {
                 children = children.Where(c => c.Name.Contains(filter.Name));
             }
 
-
+            // No children were found, send response with 404 status code
             if (children == null)
             {
                 return NotFound();
             }
 
+            // Send response with 200 status code and newly created child in body
             return Ok(children);
         }
 
-        // GET api/values/5
+        // GET api/child/5
         [HttpGet("{id}", Name = "GetChild")]
-        public IActionResult Get([FromRoute] int? id)
+        public IActionResult Get([FromRoute] int id)
         {
+            /*
+                This condition validates the values in model binding.
+                In this case, it validates that the id value is an integer.
+                If the following URL is requested, model validation will
+                fail - because the string of 'chicken' is not an integer -
+                and the client will receive a message to that effect.
+
+                    curl -X GET http://localhost:5000/api/child/chicken
+             */
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
@@ -72,6 +112,8 @@ namespace BagAPI.Controllers
             }
             catch (System.InvalidOperationException ex)
             {
+                
+                _log.LogDebug(ex.Message);
                 return NotFound();
             }
         }
@@ -144,7 +186,7 @@ namespace BagAPI.Controllers
           return _context.Child.Count(e => e.ChildId == kidId) > 0;
         }
 
-        // PUT api/values/5
+        // PUT api/child/5
         [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody] Child child)
         {
@@ -179,7 +221,7 @@ namespace BagAPI.Controllers
             return new StatusCodeResult(StatusCodes.Status204NoContent);
         }
 
-        // DELETE api/values/5
+        // DELETE api/child/5
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
